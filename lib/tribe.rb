e7863f61435f63d597ae68190ea23ed0b499a3e0
Maximiliano Pombo <maxipombo@gmail.com>
2016-03-06 16:55:02 -0300

    Puts tribe creation

--- lib/tribe.rb
+++ lib/tribe.rb
@@ -1,7 +1,12 @@
 class Tribe
   attr_reader :name, :members # Tribe has name and members
 
-  def initialize(name)
-    @name = name
+  def initialize(options = {})
+    @name = options[:name]
+    @members = options[:members]
+
+    # Puts tribe creation
+    puts "Tribe #{@name} has been created"
   end
+  
 end
